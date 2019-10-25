b23a65c624df32a8f9763da7eb23b0cc827e4eac
Michael Kuehl <michael@ratchet.cc>
2019-10-25 09:43:49 +0200

    changed name of the service in info

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -2,7 +2,7 @@ package api
 
 const (
 	// FullName is the name of the service
-	FullName string = "shadowman-the-bot (shtb), url-shortener"
+	FullName string = "lnkk.social, url-shortener"
 
 	// Version is the human readable version string of this build
 	Version string = "1.0"
